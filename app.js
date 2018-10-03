26548b78676ea8e4be753636bd1b280fdb86a690
Hack Reactor Student <student@hackreactor.com>
2018-10-03 13:34:44 -0700

    Finshed

--- app.js
+++ app.js
@@ -6,11 +6,34 @@
 class GroceryListItem extends React.Component {
   constructor(props) {
     super(props);
+    
+    //set initial state for the item
+    this.state = {
+      done: false,
+      hover: false
+    };
+  }
+
+  clicky () {
+    this.setState ({
+      done: !this.state.done,
+      hover: !this.state.hover
+    });
+    window.alert('Please stop clicking on me!');
+
   }
 
   render() {
+
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none',
+
+    };
+
+    //style={style} sets class; 
     return (
-      <li>{this.props.item}</li>
+      <li style={style} onClick={this.clicky.bind(this)}>{this.props.item}</li>
+      //Why do you need to bind to this in order for it to show? 
     );
   }
 };
@@ -32,3 +55,11 @@ var App = () => (
 );
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+
+  //es5 
+        //if (this.state.done) {
+          //*css method* 'line-through
+        // } else {
+          // 'none'
+        // }
